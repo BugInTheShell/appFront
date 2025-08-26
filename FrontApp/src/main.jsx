34628d2f1a5c51880b4e2342aa0bd9c8451e815c
BugInTheShell <aboutadev@gmail.com>
2025-08-26 00:07:47 -0600

    add: (Uso de react router doom)

--- FrontApp/src/main.jsx
+++ FrontApp/src/main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './app/store.js'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import Inicio from './pages/Dashboard/index.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/inicio" element={<Inicio />}/>
+    </Routes>
+    </Provider>
+  </BrowserRouter>
 )
